159ed69d847950432fb16804a0db546db0e18321
Tim Moreton <tmoreton89@gmail.com>
2026-08-21 21:35:34 -0400

    Initial Commit

---
+++ DocScan/DocScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DocScanApp.swift
+//  DocScan
+//
+//  Created by Homelab on 8/21/26.
+//
+
+import SwiftUI
+
+@main
+struct DocScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
