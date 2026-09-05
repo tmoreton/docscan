edcc8cd0050fc81b829f66259c2026edc27cb9be
Tim Moreton <tmoreton89@gmail.com>
2026-09-05 08:38:31 -0400

    Move PaperIndex site to GitHub Pages

--- site/worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, environment) {
-    if (environment.ASSETS?.fetch) {
-      return environment.ASSETS.fetch(request);
-    }
-
-    return new Response("PaperIndex site asset not found.", { status: 404 });
-  },
-};
